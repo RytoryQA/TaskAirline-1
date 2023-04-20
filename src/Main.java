2d7952b1c96e019b3d5cdf866564f7001430ba84
RytoryQA <vikaryzhkina89@gmail.com>
2023-04-20 17:04:54 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int oneMile = 20;
+        int amount = 3600;
+        int bonusMile = amount / oneMile;
+        System.out.println("Количество бонусных миль: " + bonusMile);
+    }
+}
